06e7bc1466af4f7beb5189aaad42e2b9b0c4d6f0
Austin <austincgomez@gmail.com>
2015-10-12 00:04:28 -0500

    Creation of the main program
    Note: Program is not fully functional yet.

---
+++ lionseeker.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to Lionseeker: A brand new open source mass SQL injection checker" << endl;
+	cout << "This is the initial commit, development is in the beta1 branch until beta is launched" << endl;
+	return 0;
+}
