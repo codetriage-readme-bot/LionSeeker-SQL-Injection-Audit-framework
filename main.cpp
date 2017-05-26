c3e9c65b2feb5a7f643fbaafcac39d951b0eb4d0
Austin <austincgomez@gmail.com>
2017-05-26 03:39:12 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,23 +7,35 @@
 
 using namespace std;
 
+void loopBack()
+{
+	std::cout << "Hm" << endl;
+}
+
 void contribute()
 {
 std::cout << "You reached the contribute menu" << endl;
 }
+
 void license()
 {
-std::cout << "You license menu." << endl;
+	string openLicense;
+std::cout << "This program is operating under the MIT License" << endl;
+std::cout << "In addition, we ask that you refrain from using this Lionseeker for malciious purposes." << endl;
+std::cout << "A copy of this license can be found in the files and on our Github page" << endl;
+std::cout << "Please restart the program" << endl;
+
 }
 void help()
 {
-std::cout << "You reached the help menu. The most important of all functions" << endl;
+std::cout << "You reached the help menu." << endl;
 }
 
 void start()
 {
-std::cout << "You reached the start menu. The most important of all other functions" << endl;
+	std::cout << "Welcome to the Start Page." << endl;
 }
+
 
 void wrong_answer(string incorrect) {
   std::cout << "You entered the value" << incorrect << "This is invalid. Please check your keyboard carefully and try again" << std::endl;
@@ -31,42 +43,60 @@ void wrong_answer(string incorrect) {
 
 int main() {
     string initial_input;
+	int InputValidation = 0;
+	//const string potentialInputs[5] = { "-s","-h","-l","-c" };
+	std::cout << InputValidation << endl;
+	while (InputValidation == 0)
+	{
 
-    std::cout << " Welcome to the Lionseeker Platform!" << endl;
-    std::cout << "We want to help you find SQL Injections." << endl;
-    std::cout << "We strongly discourage using our tool for malicious purposes" << endl;
-    std::cout << "type -s to get started, -h for help,-l license purposes, and -c if you wish to contribute" << endl;
-    std::cout << "Version 0.0.1 Early Alpha" << std::endl;
 
-    cin >> initial_input;
-    // I should likely refactor this into a case statement some time.
-    if (initial_input == "-s")
-    {
-      start();
-    }
+		std::cout << " Welcome to the Lionseeker Platform!" << endl;
+		std::cout << "We want to help you find SQL Injections." << endl;
+		std::cout << "We strongly discourage using our tool for malicious purposes" << endl;
+		std::cout << "type -s to get started, -h for help,-l license purposes, and -c if you wish to contribute" << endl;
+		std::cout << "Version 0.0.1 Early Alpha" << std::endl;
 
-    if (initial_input == "-h")
-    {
-      help();
-    }
+		cin >> initial_input;
+		if (initial_input == "-s")
+		{
+			//Goes to the start module
+			start();
+			InputValidation = 1;
+			break;
+		}
+		
+		if (initial_input == "-h")
+		{
+			// Goes to the help module
+			help();
+			InputValidation = 1;
+			break;
+		}
 
-    if (initial_input == "-l")
-    {
-      license();
-    }
+		if (initial_input == "-l")
+		{
+			// Goes to the license agreement module
+			license();
+			InputValidation = 1;
+			break;
+		}
 
-    if (initial_input == "-c")
-    {
-      contribute();
-    }
+		if (initial_input == "-c")
+		{
+			// Goes to the contribution argeement module
+			InputValidation = 1;
+			break;
+		}
+		else
+		{
+			std::cout << "The command that you entered is not valid. Please try again" << endl;
+		}
 
-    if (initial_input != "-s")
-    {
-      wrong_answer(initial_input);
 
-    }
+		std::cout << InputValidation << endl;
+	}
+	
 
-    // Implement the driver or some other testing method here. 
 
     return 0;
 }
